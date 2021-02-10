6247d89350e99102f7e3d5e63082401850b7bb22
RifqiMalik29 <malikiskandar29@gmail.com>
2021-02-10 14:54:26 +0700

    finished navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Auth from './Pages/Auth/Auth';
+import Home from './Pages/Home/Home';
+
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Navbar />
             <Switch>
-                <Route path="/" exact component={Auth} />
+                <Route path="/auth" exact component={Auth} />
+                <Route path="/" exact component={Home} />
             </Switch>
         </BrowserRouter>
     )
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import AnkasaLogo from '../images/ankasa-blue.svg';
+import SearchLogo from '../images/search.svg';
+import ActiveBar from '../images/active-bar.svg';
+
+const Navbar = () => {
+    const isSignup = true;
+    const [isActive, setIsactive] = useState(true);
+
+    return (
+        <div className="">
+            <div className="navbar-container container">
+                <div className="">
+                    <p className="ankasa"><img src={AnkasaLogo} alt="Ankasa Logo" className="ankasa-icon" />Ankasa</p>
+                </div>
+                <div className="middle-navbar">
+                    <div className="input-bg">
+                        <img src={SearchLogo} alt="Search Logo" className="search-logo" />
+                        <input placeholder="Where you want to go?" className="search-bar" />
+                    </div>
+                    <div className="navbar-link mt-2">
+                        <div className={isActive ? "active-link mx-5" : "unactive-link mx-5"} onClick={() => setIsactive(true)}>
+                            <p className="">Find Ticket</p>
+                            {isActive && <img src={ActiveBar} alt="Active Bar" className="active-bar" />}
+                        </div>
+                        <div className={isActive ? "unactive-link" : "active-link"} onClick={() => setIsactive(false)}>
+                            <p>My Booking</p>
+                            {isActive ? null : <img src={ActiveBar} alt="Active Bar" className="active-bar" />}
+                        </div>
+                    </div>
+                </div>
+                {
+                    isSignup ? (
+                        <button type="button" className="navbar-signup">Sign up</button>
+                    ) : (
+                        <p>User Profile</p>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
