9d3348237dc6be7e7f441c54144b166bd11a14ca
RifqiMalik29 <malikiskandar29@gmail.com>
2021-02-11 14:58:54 +0700

    footer finished

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Auth from './Pages/Auth/Auth';
 import Home from './Pages/Home/Home';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="/auth" exact component={Auth} />
                 <Route path="/" exact component={Home} />
             </Switch>
+            <Footer />
         </BrowserRouter>
     )
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import AnkasaLogo from '../images/ankasa-blue.svg';
+import GooglePlay from '../images/google-play.svg';
+import AppStore from '../images/app-store.svg';
+import Facebook from '../images/facebook-footer.svg';
+import Twitter from '../images/twitter.svg';
+import Instagram from '../images/instagram.svg';
+import Youtube from '../images/youtube.svg';
+import MapPin from '../images/map-pin.svg';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="container footer-container">
+                <div className="footer1-container">
+                    <div className="footer1-item">
+                        <p className="ankasa"><img src={AnkasaLogo} alt="Ankasa Logo" className="ankasa-icon" />Ankasa</p>
+                        <p className="ankasa-desc pt-3">Find your Flight and explore the <br /> world with us. We will take care of the rest</p>
+                    </div>
+                    <p className="reserved">© Ankasa.  All Rights Reserved.</p>
+                </div>
+                <div className="footer2-container">
+                    <p className="feature">Features</p>
+                    <div className="footer2-item">
+                        <p className="features">Find Ticket</p>
+                        <p className="features">My Booking</p>
+                        <p className="features">Chat</p>
+                        <p className="features">Notification</p>
+                    </div>
+                </div>
+                <div className="footer3-container">
+                    <p className="feature">Download Ankasa app</p>
+                    <img src={GooglePlay} alt="Google Play" className="mb-3 mt-3" />
+                    <img src={AppStore} alt="App Store" />
+                </div>
+                <div className="footer4-container">
+                    <p className="feature">Follow Us</p>
+                    <div className="footer4-item pt-3">
+                        <img src={Facebook} alt="Facebook" className="ml-2" />
+                        <img src={Twitter} alt="Twitter" className="mx-4" />
+                        <img src={Instagram} alt="Instagram" className="mr-4" />
+                        <img src={Youtube} alt="Youtube" />
+                    </div>
+                    <div className="location">
+                        <img src={MapPin} alt="Map Pin" />
+                        <p className="location-detail pt-3">Jakarta Indonesia</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
